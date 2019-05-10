c6ff1119da8c976b587716524a7149fcde32151b
saravankumarpa <saravanpa@gmail.com>
2019-05-10 09:45:51 -0700

    vupress integrating

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/Header/',
+      '/Footer/',
+      '/Layout/',
+      '/FeeCalculator/',
+
+    ]
+  }
+}
---
+++ docs/FeeCalculator/README.md
@@ -0,0 +1,3 @@
+# SBC-Fee Calculator
+
+
